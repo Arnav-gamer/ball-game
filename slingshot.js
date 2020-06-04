02b40e5d8a948264bed68514c399bfc0e1983d18
Arnav <65177291+Arnav-gamer@users.noreply.github.com>
2020-06-04 18:25:21 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,11 @@
+class Slingshot{
+    constructor(bodyA,bodyB){
+        var options={
+          isStatic=1.5
+        }
+
+    }
+    this.sling = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.sling);
+}
